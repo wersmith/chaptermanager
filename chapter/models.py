808dcd3ce8498effd247bce9bb80fcf424c7d577
Wesley <wesley.smith@betasigmapsi.org>
2018-05-01 22:12:23 -0400

    more chapter model fields added

--- chapter/models.py
+++ chapter/models.py
@@ -1,44 +1,40 @@
 from django.db import models
 
 # Create your models here.
-class Chapter(models.Model):
+class ChapterInfo(models.Model):
     # Fields
-    CHAPTER_CHOICES = (Alpha, Beta, Delta, Epsilon, Zeta, Eta, Iota, Nu, Omega, Alpha-Alpha )
+    CHAPTER_CHOICES = ('Alpha', 'Beta', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Iota', 'Nu', 'Omega', 'Alpha-Alpha')
     chapter_name = models.CharField(max_length=20, choices=CHAPTER_CHOICES, default="N/A")
     chapter_status = models.CharField(max_length=20, help_text="chapter")
 
 
-
-    # Methods
-
-    def __str__(self):
-        """
-        String for representing the MyModelName object (in Admin site etc.)
-        """
-        return self.field_name
+class ActivationInfo(models.Model):
+    activation_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    class_number =  models.AutoField()
+    submitted_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    chapter = models.ForeignKey('ChapterInfo', on_delete=models.CASCADE)
 
 
 
+class MemberChurchInfo(models.Model):
+    SYNOD_CHOICES = ('LCMS', 'ELCA', 'NALC', 'WELS', 'OTHER')
 
-
-
-
+    church_name = models.CharField(max_length=50, default="N/A")
+    home_pastor = models.Charfield(max_length=50, default="N/A")
+    church_address = models.Charfield(max_length=50, default="N/A")
+    church_state = models.CharField(max_length=2, default="N/A")
+    church_zip = models.CharField(max_length=5, default="N/A")
+    church_synod = models.CharField(max_length=5, choices = SYNOD_CHOICES, default="N/A")
 
 
 
 class Member(models.Model):
     # Fields
+    STATUS_CHOICES = ('Active', 'Inactive', 'Alumni')
+
     first_name = models.CharField(max_length=20, help_text="Enter first name")
     last_name = models.CharField(max_length=20, help_text="Enter last name")
     home_street_address = models.CharField(max_length=50, help_text="Enter Home Address")
-    chapter = Foreign
-
-
-
-    # Methods
-
-    def __str__(self):
-        """
-        String for representing the MyModelName object (in Admin site etc.)
-        """
-        return self.field_name
+    chapter = models.ForeignKey('ChapterInfo', on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="N/A")
+    church_info = models.ForeignKey('MemberChurchInfo', on_delete=models.CASCADE)
